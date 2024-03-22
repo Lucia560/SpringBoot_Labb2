7c828841ac36d35fdf8815a273c38256020e1c36
Lucia Predescu <luciana-579@hotmail.com>
2024-03-22 18:59:47 +0100

    initial commit

---
+++ src/test/java/org/example/springboot_labb2/SpringBootLabb2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springboot_labb2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootLabb2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
