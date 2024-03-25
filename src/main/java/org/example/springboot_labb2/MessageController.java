15a32886b98b30de7784ab9e7abf35bdf2650404
Trung Ma <trung.ma@iths.se>
2024-03-25 17:05:51 +0100

    Added PostMapping

--- src/main/java/org/example/springboot_labb2/MessageController.java
+++ src/main/java/org/example/springboot_labb2/MessageController.java
@@ -3,6 +3,7 @@ package org.example.springboot_labb2;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -25,5 +26,11 @@ public class MessageController {
     public ResponseEntity<Message> getMessageById(@PathVariable Long id) {
         Optional<Message> messageOptional = messageRepository.findById(id);
         return messageOptional.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public ResponseEntity<Message> createMessage(@RequestBody Message message) {
+        Message savedMessage = messageRepository.save(message);
+        return ResponseEntity.created(URI.create("/api/messages/" + savedMessage.getId())).body(savedMessage);
     }
 }
