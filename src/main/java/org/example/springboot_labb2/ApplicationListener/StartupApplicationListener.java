27e679cb8dc194f557730cf4344ab25a6417a497
Rishad Mahmudi <rismah1102@gmail.com>
2024-03-26 22:36:07 +0100

    Deleted old branch and copied code to new branch

---
+++ src/main/java/org/example/springboot_labb2/ApplicationListener/StartupApplicationListener.java
@@ -0,0 +1,23 @@
+package org.example.springboot_labb2.ApplicationListener;
+
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+import java.util.logging.Logger;
+@Component
+public class StartupApplicationListener {
+
+    private static final Logger LOG
+            = Logger.getLogger(StartupApplicationListener.class.getName());
+
+    @EventListener
+    public void onApplicationEvent(ApplicationReadyEvent event) {
+        LOG.info("Application started!");
+    }
+    @EventListener
+    public void onApplicationEvent(ContextRefreshedEvent event) {
+        LOG.info("Application context has been refreshed.");
+    }
+}
