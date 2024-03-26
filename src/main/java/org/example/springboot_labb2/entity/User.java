757e9907e80bef6f718185f895ec24044ee219fe
Trung Ma <trung.ma@iths.se>
2024-03-26 10:49:57 +0100

    Moved listOfMessages to a separate class for encapsulation

--- src/main/java/org/example/springboot_labb2/entity/User.java
+++ src/main/java/org/example/springboot_labb2/entity/User.java
@@ -1,7 +1,6 @@
 package org.example.springboot_labb2.entity;
 
 import jakarta.persistence.*;
-import org.example.springboot_labb2.entity.Message;
 
 import java.util.ArrayList;
 import java.util.List;
--- src/main/java/org/example/springboot_labb2/repository/MessageListHolder.java
+++ src/main/java/org/example/springboot_labb2/repository/MessageListHolder.java
@@ -1,10 +1,10 @@
 package org.example.springboot_labb2.repository;
 
 import org.example.springboot_labb2.entity.Message;
-import org.springframework.data.repository.ListCrudRepository;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public interface MessageRepository extends ListCrudRepository <Message, Long> {
+public class MessageListHolder {
+    static List<Message> listOfMessages = new ArrayList<>();
 }
