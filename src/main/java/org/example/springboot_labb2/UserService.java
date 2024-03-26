5c17e4675b75718e14a47758616da036e4493d4e
Trung Ma <trung.ma@iths.se>
2024-03-26 09:53:55 +0100

    Created UserSerice class

---
+++ src/main/java/org/example/springboot_labb2/UserService.java
@@ -0,0 +1,42 @@
+package org.example.springboot_labb2;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public Optional<User> getUserById(UUID id) {
+        return userRepository.findById(id);
+    }
+
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public User updateUser(UUID id, User user) {
+        if (userRepository.existsById(id)) {
+            user.setId(id);
+            return userRepository.save(user);
+        }
+        return null;
+    }
+
+    public void deleteUser(UUID id) {
+        userRepository.deleteById(id);
+    }
+}
