bc955a5e0698df572470b9faaaaf71f4ed350fae
Trung Ma <trung.ma@iths.se>
2024-03-26 10:14:48 +0100

    Created MessageService class

---
+++ src/main/java/org/example/springboot_labb2/MessageService.java
@@ -0,0 +1,41 @@
+package org.example.springboot_labb2;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MessageService {
+
+    private final MessageRepository messageRepository;
+
+    @Autowired
+    public MessageService(MessageRepository messageRepository) {
+        this.messageRepository = messageRepository;
+    }
+
+    public List<Message> getAllMessages() {
+        return messageRepository.findAll();
+    }
+
+    public Optional<Message> getMessageById(Long id) {
+        return messageRepository.findById(id);
+    }
+
+    public Message createMessage(Message message) {
+        return messageRepository.save(message);
+    }
+
+    public Message updateMessage(Long id, Message message) {
+        if (messageRepository.existsById(id)) {
+            message.setId(id);
+            return messageRepository.save(message);
+        }
+        return null;
+    }
+
+    public void deleteMessage(Long id) {
+        messageRepository.deleteById(id);
+    }
+}
