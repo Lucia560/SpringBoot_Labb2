9e5fcd97aa16468cfd13744e3228e13e0b248a32
Trung Ma <trung.ma@iths.se>
2024-03-27 13:14:36 +0100

    Changed UUID to Id

--- src/main/java/org/example/springboot_labb2/service/UserService.java
+++ src/main/java/org/example/springboot_labb2/service/UserService.java
@@ -6,7 +6,6 @@ import org.example.springboot_labb2.repository.UserRepository;
 import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
-import java.util.UUID;
 
 @Service
 public class UserService {
@@ -21,7 +20,7 @@ public class UserService {
         return userRepository.findAll();
     }
 
-    public Optional<User> getUserById(UUID id) {
+    public Optional<User> getUserById(Long id) {
         return userRepository.findById(id);
     }
 
@@ -29,7 +28,7 @@ public class UserService {
         return userRepository.save(user);
     }
 
-    public User updateUser(UUID id, User user) {
+    public User updateUser(Long id, User user) {
         if (userRepository.existsById(id)) {
             user.setId(id);
             return userRepository.save(user);
@@ -37,7 +36,7 @@ public class UserService {
         throw new ResourceNotFoundException("User with id " + id + " not found");
     }
 
-    public void deleteUser(UUID id) {
+    public void deleteUser(Long id) {
         userRepository.deleteById(id);
     }
 }
