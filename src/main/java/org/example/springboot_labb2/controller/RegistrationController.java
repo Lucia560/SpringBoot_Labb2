17a6676cbc2714ed987259b879876cecf8dbe31c
Trung Ma <trung.ma@iths.se>
2024-03-28 13:17:22 +0100

    added thymeleaf into my htmlfile and created registrationcontroller

---
+++ src/main/java/org/example/springboot_labb2/controller/RegistrationController.java
@@ -0,0 +1,28 @@
+package org.example.springboot_labb2.controller;
+
+import org.example.springboot_labb2.entity.User;
+import org.example.springboot_labb2.repository.UserRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class RegistrationController {
+
+    private final UserRepository userRepository;
+
+    public RegistrationController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping("/register")
+    public String showRegistrationForm() {
+        return "registration";
+    }
+
+    @PostMapping("/register")
+    public String registerUser(User user) {
+        userRepository.save(user);
+        return "redirect:/login";
+    }
+}
