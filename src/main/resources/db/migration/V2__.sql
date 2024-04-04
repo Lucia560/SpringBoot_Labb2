fdaca2fffb289bbd7f677428053464187ab1f69a
Lucia Predescu <luciana-579@hotmail.com>
2024-04-04 11:13:59 +0200

    Modify v2 flyway

--- src/main/resources/db/migration/V2__.sql
+++ src/main/resources/db/migration/V2__.sql
@@ -1,3 +1,8 @@
-
 ALTER TABLE messages
-    ADD status_private BIT(1) NOT NULL DEFAULT 0;
+    ADD status_private BIT(1) NULL;
+
+UPDATE messages
+SET status_private = 0
+WHERE status_private IS NULL;
+ALTER TABLE messages
+    MODIFY status_private BIT(1) NOT NULL;
